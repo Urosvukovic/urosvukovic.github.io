d76384cde7721e976fbb712849991d022572f239
unknown <urosvukovic1989@gmail.com>
2020-09-27 13:50:47 +0200

    added builded project

---
+++ precache-manifest.46e77acf97e9030535e2b92344df304a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2f6a2d6e7b201ec4203660c526f26b",
+    "url": "/react-filterList/index.html"
+  },
+  {
+    "revision": "6d26a703fa34e9db0890",
+    "url": "/react-filterList/static/css/main.6682c62b.chunk.css"
+  },
+  {
+    "revision": "b198c552473e02af7363",
+    "url": "/react-filterList/static/js/2.e2911b2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-filterList/static/js/2.e2911b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d26a703fa34e9db0890",
+    "url": "/react-filterList/static/js/main.e673ea66.chunk.js"
+  },
+  {
+    "revision": "d70556518960099c2ddc",
+    "url": "/react-filterList/static/js/runtime-main.6fe80153.js"
+  },
+  {
+    "revision": "995753a780858287de975bc30aca274d",
+    "url": "/react-filterList/static/media/man.995753a7.jpg"
+  },
+  {
+    "revision": "65b67c743f74d3e881827e18cc2d18f3",
+    "url": "/react-filterList/static/media/woman.65b67c74.jpg"
+  }
+]);
